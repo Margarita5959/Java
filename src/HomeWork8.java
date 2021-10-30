3df2737f55c8b451db25f958468a6b119364ac4c
margarita <margarita-kolobkova@yandex.ru>
2021-10-30 21:52:48 +0500

    Домашнее задание 8

---
+++ src/HomeWork8.java
@@ -0,0 +1,7 @@
+public class HomeWork8 {
+    public static void main (String [] args){
+MyWindow window = new MyWindow();
+window.initWindow();
+
+    }
+}
---
+++ src/MyWindow.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyWindow extends JFrame {
+    public void initWindow () {
+        setSize (500, 500);
+        setLocation(650, 250);
+        int value = WindowConstants.EXIT_ON_CLOSE;
+        setDefaultCloseOperation(value);
+
+        GridLayout layout = new GridLayout(1, 2);
+        JPanel panel = new JPanel(layout);
+
+        JButton firstButton = new JButton("Выход");
+        JButton secondButton = new JButton("Кнопка");
+        panel.add(firstButton);
+        panel.add (secondButton);
+
+        add (panel, BorderLayout.SOUTH);
+
+
+
+        setVisible(true);
+
+    }
+}
